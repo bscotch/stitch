983e18d653c3364cbc6fb82bb7a0dd150eed4818
Adam Coster <adam@bscotch.net>
2020-09-10 16:55:30 -0500

    feat: Add methods to the Resource Class to get the resource type.

--- src/lib/components/Gms2Resource.ts
+++ src/lib/components/Gms2Resource.ts
@@ -1,4 +1,6 @@
 import { YypResource } from "../../types/YypComponents";
+import {Inverted} from "../../types/Utility";
+import {invert} from "lodash";
 
 export class Gms2Resource {
 
@@ -8,7 +10,55 @@ export class Gms2Resource {
     this.#data = {...option};
   }
 
+  /**
+   * Name of the parent directory for this resource type,
+   * which is also the first part of its "path" field value.
+   * E.g. {path:"sprites/mySprite/mySprite.yy"} yields "sprites"
+   */
+  get pathType(){
+    return this.#data.id.path.split('/')[0] as keyof Gms2ResourcePathTypeToInternalType;
+  }
+
+  /**
+   * Gamemaker's internal naming for this resource type,
+   * used in the "resourceType" fields of the .yy files.
+   */
+  get internalType(){
+    return Gms2Resource.pathTypeToInternalType[this.pathType];
+  }
+
   get dehydrated(): YypResource{
     return {...this.#data};
   }
+
+  static get pathTypeToInternalType(){
+    return {
+      animcurves: 'GMAnimCurve',
+      configs: 'GMConfig',
+      datafiles: 'GMIncludedFile',
+      extensions: 'GMExtension',
+      fonts: 'GMFont',
+      notes: 'GMNotes',
+      objects: 'GMObject',
+      options: 'GMOptions',
+      paths: 'GMPath',
+      rooms: 'GMRoom',
+      scripts: 'GMScript',
+      sequences: 'GMSequence',
+      shaders: 'GMShader',
+      sounds: 'GMSound',
+      sprites: 'GMSprite',
+      tilesets: 'GMTileSet',
+      timelines: 'GMTimeline',
+      views: 'GMFolder',
+    } as const;
+  }
+
+  static get internalTypeToPathType(){
+    return invert(Gms2Resource.pathTypeToInternalType) as Inverted<typeof Gms2Resource.pathTypeToInternalType>;
+  }
 }
+
+
+export type Gms2ResourcePathTypeToInternalType = typeof Gms2Resource.pathTypeToInternalType;
+export type Gms2ResourceInternalTypeToPathType = Inverted<Gms2ResourcePathTypeToInternalType>;
---
+++ src/types/Utility.ts
@@ -0,0 +1,10 @@
+
+/** @see {@link https://stackoverflow.com/questions/56415826/is-it-possible-to-precisely-type-invert-in-typescript} */
+
+type KeyFromValue<V, T extends Record<PropertyKey, PropertyKey>> = {
+  [K in keyof T]: V extends T[K] ? K : never
+}[keyof T];
+
+export type Inverted<T extends Record<PropertyKey, PropertyKey>> = {
+  [V in T[keyof T]]: KeyFromValue<V, T>
+};
