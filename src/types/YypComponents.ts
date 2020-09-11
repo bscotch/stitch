c8f0162ef57ef4b06bb39f5c2c52c8873422b4a9
Shi Chen <shi@butterscotch-shenanigans.com>
2020-09-11 14:14:19 -0500

    fix: Add configvalues field to each type that uses it in the yyp types.

--- src/types/YypComponents.ts
+++ src/types/YypComponents.ts
@@ -31,38 +31,63 @@ export interface YypRoomOrder {
 }
 
 export interface YypFolder {
-  name:string,
-  tags:string,
-  folderPath:string,
-  order:number,
-  resourceType:"GMFolder"
-  resourceVersion:"1.0",
+  name: string,
+  tags: string,
+  folderPath: string,
+  order: number,
+  resourceType: "GMFolder"
+  resourceVersion: "1.0",
+}
+
+type StringifiedValues<AnObject> = { [key in keyof AnObject]: string }
+
+/**
+ * Can be used in:
+ *  1. GMSound for audioGroupId
+ *  2. GMExtension for copyToTargets
+ *  3. GMExtension for copyToTargets for "files"
+ *  4. TextureGroups for all keys
+ *  4. AudioGroups
+ *  5. GMSprite for textureGroupId
+ *  6. GMAndroidOptions(and alike) for everything
+ *  TODO: GMSprite and GMSound has more limited keys. Should we create a separate interface?
+ */
+export interface ConfigValue<Component> {
+  [configName: string]: Partial<StringifiedValues<Component>>
 }
 
 export interface YypAudioGroup {
-  name:string,
+  ConfigValues?: ConfigValue<YypAudioGroup>,
+  name: string,
   targets: BigInt,
-  resourceType:"GMAudioGroup",
-  resourceVersion:"1.0",
+  resourceType: "GMAudioGroup",
+  resourceVersion: "1.0",
 }
 
+
 export interface YypTextureGroup {
-  name:string,
+  ConfigValues?: ConfigValue<YypTextureGroup>,
+  name: string,
+  groupParent: {
+    name: string,
+    path: string
+  },
   isScaled: boolean,
   autocrop: boolean,
   border: number,
   mipsToGenerate: number,
   targets: BigInt,
-  resourceType:"GMTextureGroup",
-  resourceVersion:"1.0",
+  resourceType: "GMTextureGroup",
+  resourceVersion: "1.0",
 }
 
 export interface YypInludedFiles {
-  name:string,
-  CopyToMask:number,
-  filePath:string,
-  resourceType:"GMIncludedFile",
-  resourceVersion:"1.0",
+  ConfigValues?: ConfigValue<YypInludedFiles>,
+  name: string,
+  CopyToMask: number | string,
+  filePath: string,
+  resourceType: "GMIncludedFile",
+  resourceVersion: "1.0",
 }
 
 /** Raw YYP Content as plain objects (read directly from file and JSON parsed) */
