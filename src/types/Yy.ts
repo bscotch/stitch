4f41f174893af3ab0fcdd1c25c84c7004efc49c0
Adam Coster <adam@bscotch.net>
2020-09-15 17:31:57 -0500

    feat: Draft typings for Sprite .yy files.

--- src/types/Yy.ts
+++ src/types/Yy.ts
@@ -55,78 +55,174 @@ export interface YySound extends YyData {
   resourceType: "GMSound",
 }
 
+enum SpriteCollisionKind {
+  Precise,
+  Rectangle,
+  Ellipse,
+  Diamond,
+  PrecisePerFrame,
+  RectangleWithRotation,
+}
+
+enum SpriteBoundingBoxMode {
+  Automatic,
+  FullImage,
+  Manual,
+}
+
+enum SpriteOrigin {
+  TopLeft,
+  TopCenter,
+  TopRight,
+  MiddleLeft,
+  MiddleCenter,
+  MiddleRight,
+  BottomLeft,
+  BottomCenter,
+  BottomRight
+}
+
+enum SpritePlaybackSpeedType {
+  FramesPerSecond,
+  FramesPerGameFrame,
+}
+
+interface SpriteImage {
+  FrameId:{
+    name: string,
+    /** Path to the sprite's .yy file */
+    path:string
+  },
+  LayerId:{
+    /**
+     * Name of the layer. Corresponds to an image in each layer folder,
+     * and should be found in once in *each frame*. Must be found in the
+     * sprite's root "layers" list.
+     */
+    name:string,
+    /** Path to the sprite's .yy file */
+    path:string
+  },
+  resourceVersion:"1.0",
+  name:"",
+  /** An empty array (there seems to be no way to add tags to frames) */
+  tags:[],
+  resourceType:"GMSpriteBitmap"
+}
+
+interface SpriteCompositeImage extends Omit<SpriteImage,'LayerId'|'name'> {
+  LayerId:null,
+  name:"composite",
+}
+
+interface SpriteFrame {
+  /** Image created by flattening layers. */
+  compositeImage: SpriteCompositeImage,
+  /** One image per layer. */
+  images: SpriteImage[],
+  /** The parent sprite, same as the sprite's ID from the YYP. */
+  parent:{
+    name:string,
+    path:string
+  },
+  resourceVersion:"1.0",
+  /**
+   * Unique GUID. Matches the name of an image file (+'.png')
+   * that sits alongside the .yy file. Also matches a corresponding
+   * folder name inside the "layers" folder. The Composite image
+   * and each one listed in 'images' all have the same value here
+   * for their "FrameId.name" field.
+   */
+  name:string,
+  tags:[],
+  resourceType:"GMSpriteFrame"
+}
+
 export interface YySprite extends YyData {
-  // "bboxMode": 0,
-  // "collisionKind": 1,
-  // "type": 0,
-  // "origin": 4,
-  // "preMultiplyAlpha": false,
-  // "edgeFiltering": false,
-  // "collisionTolerance": 0,
-  // "swfPrecision": 2.525,
-  // "bbox_left": 9,
-  // "bbox_right": 56,
-  // "bbox_top": 0,
-  // "bbox_bottom": 59,
-  // "HTile": false,
-  // "VTile": false,
-  // "For3D": false,
-  // "width": 64,
-  // "height": 64,
-  // "textureGroupId": {
-  //   "name": "the_texture_group",
-  //   "path": "texturegroups/the_texture_group",
-  // },
-  // "swatchColours": null,
-  // "gridX": 0,
-  // "gridY": 0,
-  // "frames": [
-  //   {"compositeImage":{"FrameId":{"name":"98c41232-eb8d-41fc-a6d9-156eafb4d651","path":"sprites/sprite/sprite.yy",},"LayerId":null,"resourceVersion":"1.0","name":"","tags":[],"resourceType":"GMSpriteBitmap",},"images":[
-  //       {"FrameId":{"name":"98c41232-eb8d-41fc-a6d9-156eafb4d651","path":"sprites/sprite/sprite.yy",},"LayerId":{"name":"471a5749-b8f8-4630-856a-bdce7ff709b2","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","name":"","tags":[],"resourceType":"GMSpriteBitmap",},
-  //     ],"parent":{"name":"sprite","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","name":"98c41232-eb8d-41fc-a6d9-156eafb4d651","tags":[],"resourceType":"GMSpriteFrame",},
-  //   {"compositeImage":{"FrameId":{"name":"34126976-a5d5-4f2a-988b-384c47e1f81e","path":"sprites/sprite/sprite.yy",},"LayerId":null,"resourceVersion":"1.0","name":"","tags":[],"resourceType":"GMSpriteBitmap",},"images":[
-  //       {"FrameId":{"name":"34126976-a5d5-4f2a-988b-384c47e1f81e","path":"sprites/sprite/sprite.yy",},"LayerId":{"name":"471a5749-b8f8-4630-856a-bdce7ff709b2","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","name":"","tags":[],"resourceType":"GMSpriteBitmap",},
-  //     ],"parent":{"name":"sprite","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","name":"34126976-a5d5-4f2a-988b-384c47e1f81e","tags":[],"resourceType":"GMSpriteFrame",},
-  // ],
-  // "sequence": {
-  //   "spriteId": {"name":"sprite","path":"sprites/sprite/sprite.yy",},
-  //   "timeUnits": 1,
-  //   "playback": 1,
-  //   "playbackSpeed": 30.0,
-  //   "playbackSpeedType": 1,
-  //   "autoRecord": true,
-  //   "volume": 1.0,
-  //   "length": 2.0,
-  //   "events": {"Keyframes":[],"resourceVersion":"1.0","resourceType":"KeyframeStore<MessageEventKeyframe>",},
-  //   "moments": {"Keyframes":[],"resourceVersion":"1.0","resourceType":"KeyframeStore<MomentsEventKeyframe>",},
-  //   "tracks": [
-  //     {"name":"frames","spriteId":null,"keyframes":{"Keyframes":[
-  //           {"id":"c49cf451-e332-4284-88ad-605b9b54c139","Key":0.0,"Length":1.0,"Stretch":false,"Disabled":false,"IsCreationKey":false,"Channels":{"0":{"Id":{"name":"98c41232-eb8d-41fc-a6d9-156eafb4d651","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","resourceType":"SpriteFrameKeyframe",},},"resourceVersion":"1.0","resourceType":"Keyframe<SpriteFrameKeyframe>",},
-  //           {"id":"ea220aa2-106b-4e02-a07f-286e4079a42b","Key":1.0,"Length":1.0,"Stretch":false,"Disabled":false,"IsCreationKey":false,"Channels":{"0":{"Id":{"name":"34126976-a5d5-4f2a-988b-384c47e1f81e","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","resourceType":"SpriteFrameKeyframe",},},"resourceVersion":"1.0","resourceType":"Keyframe<SpriteFrameKeyframe>",},
-  //         ],"resourceVersion":"1.0","resourceType":"KeyframeStore<SpriteFrameKeyframe>",},"trackColour":0,"inheritsTrackColour":true,"builtinName":0,"traits":0,"interpolation":1,"tracks":[],"events":[],"modifiers":[],"isCreationTrack":false,"resourceVersion":"1.0","tags":[],"resourceType":"GMSpriteFramesTrack",},
-  //   ],
-  //   "visibleRange": null,
-  //   "lockOrigin": false,
-  //   "showBackdrop": true,
-  //   "showBackdropImage": false,
-  //   "backdropImagePath": "",
-  //   "backdropImageOpacity": 0.5,
-  //   "backdropWidth": 1366,
-  //   "backdropHeight": 768,
-  //   "backdropXOffset": 0.0,
-  //   "backdropYOffset": 0.0,
-  //   "xorigin": 32,
-  //   "yorigin": 32,
-  //   "eventToFunction": {},
-  //   "eventStubScript": null,
-  //   "parent": {"name":"sprite","path":"sprites/sprite/sprite.yy",},
-  //   "resourceVersion": "1.3",
-  //   "name": "sprite",
-  //   "tags": [],
-  //   "resourceType": "GMSequence",
-  // },
-  // "layers": [
-  //   {"visible":true,"isLocked":false,"blendMode":0,"opacity":100.0,"displayName":"default","resourceVersion":"1.0","name":"471a5749-b8f8-4630-856a-bdce7ff709b2","tags":[],"resourceType":"GMImageLayer",},
-  // ],
-  "resourceType": "GMSprite"
+  bboxMode: SpriteBoundingBoxMode,
+  collisionKind: SpriteCollisionKind,
+  /** (What is this?) */
+  type: 0,
+  origin: SpriteOrigin,
+  /** (Default true.) */
+  preMultiplyAlpha: boolean,
+  /** (Default true.) */
+  edgeFiltering: boolean,
+  /** 0-255. Only meaningful if collision type is "Precise". */
+  collisionTolerance: number,
+  /** (What is this?) */
+  swfPrecision: 2.525,
+  bbox_left: number,
+  bbox_right: number,
+  bbox_top: number,
+  bbox_bottom: number,
+  /** (Default false.) Horizontally tiled */
+  HTile: boolean,
+  /** (Default false.) Vertically tiled */
+  VTile: boolean,
+  /** (Default false.) Used for 3d (not sure how set...) */
+  For3D: boolean,
+  width: number,
+  height: number,
+  /** Matches the texture's id from the YYP file */
+  textureGroupId: {
+    name: string,
+    path: string,
+  },
+  /** (What is this?) */
+  swatchColours: null,
+  /** (What is this?) */
+  gridX: 0,
+  /** (What is this?) */
+  gridY: 0,
+  frames: SpriteFrame[],
+  sequence: {
+    /** Matches the YYP resource's 'id' value */
+    spriteId: {
+      name:string,
+      path:string
+    },
+    /** (Default 1) What is this? */
+    timeUnits: 1,
+    /** (Default 1) What is this? */
+    playback: 1,
+    /** FPS */
+    playbackSpeed: 30.0,
+    playbackSpeedType: SpritePlaybackSpeedType,
+    /** (Default true) What is this? */
+    autoRecord: true,
+    volume: 1.0,
+    length: 2.0,
+    events: {"Keyframes":[],"resourceVersion":"1.0","resourceType":"KeyframeStore<MessageEventKeyframe>",},
+    moments: {"Keyframes":[],"resourceVersion":"1.0","resourceType":"KeyframeStore<MomentsEventKeyframe>",},
+    tracks: [
+      {"name":"frames","spriteId":null,"keyframes":{"Keyframes":[
+        {"id":"c49cf451-e332-4284-88ad-605b9b54c139","Key":0.0,"Length":1.0,"Stretch":false,"Disabled":false,"IsCreationKey":false,"Channels":{"0":{"Id":{"name":"98c41232-eb8d-41fc-a6d9-156eafb4d651","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","resourceType":"SpriteFrameKeyframe",},},"resourceVersion":"1.0","resourceType":"Keyframe<SpriteFrameKeyframe>",},
+        {"id":"ea220aa2-106b-4e02-a07f-286e4079a42b","Key":1.0,"Length":1.0,"Stretch":false,"Disabled":false,"IsCreationKey":false,"Channels":{"0":{"Id":{"name":"34126976-a5d5-4f2a-988b-384c47e1f81e","path":"sprites/sprite/sprite.yy",},"resourceVersion":"1.0","resourceType":"SpriteFrameKeyframe",},},"resourceVersion":"1.0","resourceType":"Keyframe<SpriteFrameKeyframe>",},
+      ],"resourceVersion":"1.0","resourceType":"KeyframeStore<SpriteFrameKeyframe>",},"trackColour":0,"inheritsTrackColour":true,"builtinName":0,"traits":0,"interpolation":1,"tracks":[],"events":[],"modifiers":[],"isCreationTrack":false,"resourceVersion":"1.0","tags":[],"resourceType":"GMSpriteFramesTrack",},
+    ],
+    visibleRange: null,
+    lockOrigin: false,
+    showBackdrop: true,
+    showBackdropImage: false,
+    backdropImagePath: "",
+    backdropImageOpacity: 0.5,
+    backdropWidth: 1366,
+    backdropHeight: 768,
+    backdropXOffset: 0.0,
+    backdropYOffset: 0.0,
+    xorigin: 32,
+    yorigin: 32,
+    eventToFunction: {},
+    eventStubScript: null,
+    parent: {"name":"sprite","path":"sprites/sprite/sprite.yy",},
+    resourceVersion: "1.3",
+    name: "sprite",
+    tags: [],
+    resourceType: "GMSequence",
+  },
+  layers: [
+    {"visible":true,"isLocked":false,"blendMode":0,"opacity":100.0,"displayName":"default","resourceVersion":"1.0","name":"471a5749-b8f8-4630-856a-bdce7ff709b2","tags":[],"resourceType":"GMImageLayer",},
+  ],
+  resourceType: "GMSprite"
 }
