70f192283219faf293370b07f6f86331582076f9
Shi Chen <shi@butterscotch-shenanigans.com>
2020-09-23 16:58:43 -0500

    feat: Add version display to the help text of the CLI main command.

--- src/cli/gms2.ts
+++ src/cli/gms2.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import commander from "commander";
+import version from "./lib/project-version";
 
 export const cli = commander;
 
 // Kick it off
-cli.description('Gamemaker Studio 2: Pipeline Development Kit CLI')
+cli.version(version, '-v, --version')
+  .description('Gamemaker Studio 2: Pipeline Development Kit CLI')
+  //.command("import",  "Import assets to Gamemaker Studio 2 projects.")
   .command("jsonify", "Convert every .yy and .yyp file into valid JSON.");
 
 // Do the work!
