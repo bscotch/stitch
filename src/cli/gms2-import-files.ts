94dda7c1e176cdb635575c7266c6dc405c66954e
Shi Chen <shi@butterscotch-shenanigans.com>
2020-09-27 21:27:19 -0500

    feat: Add the import file CLI command.

--- src/cli/gms2-import-files.ts
+++ src/cli/gms2-import-files.ts
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 import commander, { CommanderStatic } from "commander";
 import { oneline, undent } from "../lib/strings";
-import importSounds from "./lib/import-sounds";
 import { ImportBaseOptions } from "./lib/import-base-options";
+import importFiles from "./lib/import-files";
 
 const cli = commander;
 
 cli.description(undent`
-    Import sound assets from a file or a path.
-    If the asset does not already exists in the target project, it will be placed in the "NEW" folder.
-    Otherwise, the asset will be replaced by the source asset.`)
+  Import included file assets from a file or a path.
+  If the asset does not already exists in the target project, it will be placed in the "NEW" folder.
+  Otherwise, the asset will be replaced by the source asset.`)
   .requiredOption("--source-path <path>", oneline`
-    Path to the sound file or the folder containing the sounds files.
+    Path to the file or the folder containing the files to import.
   `)
   .option("--allow-extensions <extensions...>", oneline`
-    input one or more of the supported extensions: mp3, wav, ogg, wma. 
-    If not set, Will attempt to import all supported extensions.
+    Only allow certain extensions to be imported. 
+    If not set, Will attempt to import all files.
   `)
   .option("--target-project-path <path>", oneline`
     Path to the target Gamemaker Studio 2 project. If not set, will use the current directory.
   `)
   .parse(process.argv);
 
-importSounds(cli as ImportBaseOptions & CommanderStatic);
+importFiles(cli as ImportBaseOptions & CommanderStatic);
