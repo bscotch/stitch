c95500e997e57790b480771931dd8bf82e19f9a5
Adam Coster <adam@bscotch.net>
2020-10-01 13:46:20 -0500

    feat: Complete draft of methods to create new Sprite yy files.

--- src/lib/components/resources/Gms2Sprite.ts
+++ src/lib/components/resources/Gms2Sprite.ts
@@ -1,9 +1,14 @@
 import {
   YySprite,
   SpriteBoundingBoxMode,
+  SpriteLayerBlendMode,
   SpriteCollisionKind,
   SpriteOrigin,
   SpritePlaybackSpeedType,
+  yyDataConstants,
+  yyDataLayerConstants,
+  yyDataSequenceConstants,
+  yyDataSequenceTrackConstants,
 } from "../../../types/YySprite";
 import { assert, Gms2PipelineError } from "../../errors";
 import { Gms2Storage } from "../../Gms2Storage";
@@ -11,6 +16,21 @@ import paths from "../../paths";
 import { oneline } from "../../strings";
 import { Gms2ResourceBase, Gms2ResourceBaseParameters } from "./Gms2ResourceBase";
 import sharp from "sharp";
+import {uuidV4,uuidV5,UuidNamespace} from "../../uuid";
+
+interface SpriteFrameSource {
+  sourcePath:string,
+  name:string,
+  guid:string,
+}
+
+interface SpriteSource {
+  guid: string,
+  spriteName: string,
+  width: number,
+  height: number,
+  frames: SpriteFrameSource[]
+}
 
 export class Gms2Sprite extends Gms2ResourceBase {
 
@@ -31,35 +51,124 @@ export class Gms2Sprite extends Gms2ResourceBase {
   }
 
   protected createYyFile(){
+    // Create a frameless sprite template
+    const firstLayerId = uuidV4();
+
     const yyData: YySprite = {
-      name: this.name,
+      ...yyDataConstants,
       bboxMode: SpriteBoundingBoxMode.Automatic,
-      collisionKind: SpriteCollisionKind.Rectangle,
-      type: 0,
-      origin: SpriteOrigin.MiddleCenter,
-      preMultiplyAlpha: true,
-      edgeFiltering: true,
-      collisionTolerance: 0,
-      swfPrecision: 2.525,
-      bbox_left: 0,
-      bbox_right: 0,
-      bbox_top: 0,
       bbox_bottom: 0,
-      HTile: false,
-      VTile: false,
-      For3D: false,
-      width: 0,
-      height: 0,
-      tags: [],
-      swatchColours: null,
-      gridX: 0,
-      gridY: 0,
+      bbox_left: 0, // Should set later based on frame dims
+      bbox_right: 0,
+      bbox_top: 0, // Should set based on frame dims
+      collisionKind: SpriteCollisionKind.Rectangle,
+      origin: SpriteOrigin.Custom,
+      collisionTolerance: 0,
+      edgeFiltering: false,
+      frames: [], // Add frames later based on subimages!
+      height: 0, // Set later based on subimages!
+      layers: [
+        {
+          ...yyDataLayerConstants,
+          blendMode: SpriteLayerBlendMode.Normal,
+          displayName: "default",
+          name: firstLayerId,
+          opacity: 100,
+          isLocked: false,
+          visible: true,
+        }
+      ],
+      name: this.name,
       parent: Gms2Sprite.parentDefault,
+      preMultiplyAlpha: false,
+      sequence: {
+        ...yyDataSequenceConstants,
+        length: 0, // Update with number of frames
+        name: this.name,
+        parent:  this.id,
+        playbackSpeed: 60,
+        playbackSpeedType: SpritePlaybackSpeedType.FramesPerSecond,
+        spriteId: this.id,
+        tracks: [
+          {
+            ...yyDataSequenceTrackConstants,
+            keyframes: {
+              Keyframes: [], // Update once there are frames to add
+              resourceType: "KeyframeStore<SpriteFrameKeyframe>",
+              resourceVersion: "1.0"
+            },
+          }
+        ],
+        xorigin: 0, // Update with the origin of the sprite
+        yorigin: 0, // Update with the origin of the sprite
+      },
+      tags: [],
       textureGroupId: Gms2Sprite.textureGroupIdDefault,
-      resourceType: "GMSprite",
-      resourceVersion: "1.0",
+      width: 0
     };
     this.storage.saveJson(this.yyPathAbsolute,yyData);
+  }
+
+  /** Force the frames of this sprite to match the images within a folder (non-recursive)  */
+  async replaceFrames(subimageDirectory:string){
+    const imageData = await Gms2Sprite.getSpriteDataFromSourceDirectory(subimageDirectory,this.storage);
+    // TODO
+    // If the current Sprite has no frames,
+
+    return this;
+  }
+
+  /**
+   * Given an external folder of images, where the folder corresponds to a sprite
+   * and the images to its frames, compute the data needed to be able to make use
+   * of those frames to update/create a GMS2 Sprite resource.
+   */
+  static async getSpriteDataFromSourceDirectory(spriteDirectory:string,storage:Gms2Storage){
+    // Find the subimages
+    const subimagePaths = storage.listFiles(spriteDirectory,false,['png']);
+    assert(subimagePaths.length,`No png subimages found in ${spriteDirectory}`);
+    // Make sure all images are the same size and are actual PNGs
+    const spriteName = paths.subfolderName(spriteDirectory);
+    const spriteInfo: SpriteSource = {
+      guid: uuidV5(spriteName,UuidNamespace.Sprite),
+      spriteName,
+      width:0,
+      height:0,
+      frames: []
+    };
+    for(const subimagePath of subimagePaths){
+      const subimageInfo = await sharp(subimagePath).metadata();
+      assert(subimageInfo.format=='png',`Subimage ${subimagePath} is not in PNG format.`);
+      if(!subimageInfo.width || !subimageInfo.height){
+        throw new Gms2PipelineError(`Subimage ${subimagePath} is malformed: missing a width or height.`);
+      }
+      if(!spriteInfo.frames.length){
+        spriteInfo.width = subimageInfo.width;
+        spriteInfo.height = subimageInfo.height;
+      }
+      else{
+        assert(
+          subimageInfo.width == spriteInfo.width && subimageInfo.height == spriteInfo.height,
+          oneline`Subimage ${subimagePath} has different dimensions (${subimageInfo.width}x${subimageInfo.height})
+                  than prior subimages (${spriteInfo.width}x${spriteInfo.height})`
+        );
+      }
+      const frameName = paths.basename(subimagePath);
+      const frame = {
+        sourcePath: subimagePath,
+        name: frameName,
+        guid: uuidV5(frameName,spriteInfo.guid)
+      };
+      spriteInfo.frames.push(frame);
+    }
+    return spriteInfo;
+  }
+
+  static get textureGroupIdDefault(){
+    return {
+      name: 'Default',
+      path: "texturegroups/Default",
+    };
   }
 
   /**
@@ -69,44 +178,9 @@ export class Gms2Sprite extends Gms2ResourceBase {
    *                          search for png images within that directory
    *                          and sort them alphabetically.
    */
-  static async create(subimageDirectory:string,storage:Gms2Storage): Gms2Sprite {
-    const imageData = Gms2Sprite.getSubimageData(subimageDirectory,storage);
-    // TODO: Figure all this out!
+  static async create(subimageDirectory:string,storage:Gms2Storage,spriteName?:string): Promise<Gms2Sprite> {
+    return new Gms2Sprite(spriteName||paths.subfolderName(subimageDirectory),storage,true)
+      .replaceFrames(subimageDirectory);
   }
 
-  static async getSubimageData(subimageDirectory:string,storage:Gms2Storage){
-    // Find the subimages
-    const subimagePaths = storage.listFiles(subimageDirectory,false,['png']);
-    assert(subimagePaths.length,`No png subimages found in ${subimageDirectory}`);
-    // Make sure all images are the same size and are actual PNGs
-    const subimagesSummary = {width:0,height:0,sourcePaths:[] as string[],names:[] as string[]};
-    for(const subimagePath of subimagePaths){
-      const subimageInfo = await sharp(subimagePath).metadata();
-      assert(subimageInfo.format=='png',`Subimage ${subimagePath} is not in PNG format.`);
-      if(!subimageInfo.width || !subimageInfo.height){
-        throw new Gms2PipelineError(`Subimage ${subimagePath} is malformed: missing a width or height.`);
-      }
-      if(!subimagesSummary.names.length){
-        subimagesSummary.width = subimageInfo.width;
-        subimagesSummary.height = subimageInfo.height;
-      }
-      else{
-        assert(
-          subimageInfo.width == subimagesSummary.width && subimageInfo.height == subimagesSummary.height,
-          oneline`Subimage ${subimagePath} has different dimensions (${subimageInfo.width}x${subimageInfo.height})
-                  than prior subimages (${subimagesSummary.width}x${subimagesSummary.height})`
-        );
-      }
-      subimagesSummary.sourcePaths.push(subimagePath);
-      subimagesSummary.names.push(paths.basename(subimagePath));
-    }
-    return subimagesSummary;
-  }
-
-  static get textureGroupIdDefault(){
-    return {
-      name: 'Default',
-      path: "texturegroups/Default",
-    };
-  }
 }
