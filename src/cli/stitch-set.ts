92dbbb8d1df86087e07ab968094e9364c8fa5ced
Adam Coster <adam@bscotch.net>
2020-10-23 17:50:26 -0500

    feat: The CLI set subcommands for texture and audio groups are no longer plural, since that was confusing. BREAKING.

--- src/cli/stitch-set.ts
+++ src/cli/stitch-set.ts
@@ -4,7 +4,7 @@ const cli = commander;
 
 cli.description("Modify metadata in GameMaker Studio 2 projects.")
   .command("version", "Modify the versions for all export platforms.")
-  .command("texture-groups", "Modify texture group assignments.")
-  .command("audio-groups", "Modify audio group assignments.")
+  .command("texture-group", "Modify texture group assignments.")
+  .command("audio-group", "Modify audio group assignments.")
   .parse(process.argv);
 
