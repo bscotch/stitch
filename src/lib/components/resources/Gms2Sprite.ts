60896a1208a66f5d337de85428c122226a41a570
Adam Coster <adam@bscotch.net>
2021-03-01 14:25:29 -0600

    feat: Update all dependencies.

--- src/lib/components/resources/Gms2Sprite.ts
+++ src/lib/components/resources/Gms2Sprite.ts
@@ -5,68 +5,70 @@ import {
   yyDataSequenceDefaults,
   yyDataSequenceTrackDefaults,
   SpriteType,
-} from "../../../types/YySprite";
-import { Gms2Storage } from "../../Gms2Storage";
-import paths from "../../paths";
-import { Gms2ResourceBase, Gms2ResourceBaseParameters } from "./Gms2ResourceBase";
-import {Spritely} from "@bscotch/spritely";
-import {uuidV4} from "../../uuid";
-import { NumberFixed } from "../../NumberFixed";
-import { assert } from "../../errors";
-import { logDebug } from "../../log";
+} from "types/YySprite";
+import { Gms2Storage } from "stitch/Gms2Storage";
+import paths from "stitch/paths";
+import {
+  Gms2ResourceBase,
+  Gms2ResourceBaseParameters,
+} from "./Gms2ResourceBase";
+import { Spritely } from "@bscotch/spritely";
+import { uuidV4 } from "stitch/uuid";
+import { NumberFixed } from "stitch/NumberFixed";
+import { assert } from "stitch/errors";
+import { logDebug } from "stitch/log";
 
-const toSingleDecimalNumber = (number:number|undefined)=>{
-  return new NumberFixed(number||0,1);
+const toSingleDecimalNumber = (number: number | undefined) => {
+  return new NumberFixed(number || 0, 1);
 };
 export class Gms2Sprite extends Gms2ResourceBase {
-
   protected yyData!: YySprite; // Happens in the super() constructor
 
   constructor(...setup: Gms2ResourceBaseParameters) {
-    super("sprites",...setup);
+    super("sprites", ...setup);
   }
 
-  protected get fieldConverters(){
+  protected get fieldConverters() {
     return {
-      'sequence.volume':toSingleDecimalNumber,
-      'sequence.playbackSpeed':toSingleDecimalNumber,
-      'sequence.length':toSingleDecimalNumber,
-      'sequence.visibleRange.x': toSingleDecimalNumber,
-      'sequence.visibleRange.y': toSingleDecimalNumber,
-      'sequence.backdropXOffset': toSingleDecimalNumber,
-      'sequence.backdropYOffset': toSingleDecimalNumber,
-      'sequence.tracks.*.keyframes.Keyframes.*.Key': toSingleDecimalNumber,
-      'sequence.tracks.*.keyframes.Keyframes.*.Length': toSingleDecimalNumber,
-      'layers.*.opacity': toSingleDecimalNumber,
+      "sequence.volume": toSingleDecimalNumber,
+      "sequence.playbackSpeed": toSingleDecimalNumber,
+      "sequence.length": toSingleDecimalNumber,
+      "sequence.visibleRange.x": toSingleDecimalNumber,
+      "sequence.visibleRange.y": toSingleDecimalNumber,
+      "sequence.backdropXOffset": toSingleDecimalNumber,
+      "sequence.backdropYOffset": toSingleDecimalNumber,
+      "sequence.tracks.*.keyframes.Keyframes.*.Key": toSingleDecimalNumber,
+      "sequence.tracks.*.keyframes.Keyframes.*.Length": toSingleDecimalNumber,
+      "layers.*.opacity": toSingleDecimalNumber,
     };
   }
 
-  get isSpine(){
-    const frameId   = this.frameIds[0];
-    const atlasPath = paths.join(this.yyDirAbsolute,`${frameId}.atlas`);
+  get isSpine() {
+    const frameId = this.frameIds[0];
+    const atlasPath = paths.join(this.yyDirAbsolute, `${frameId}.atlas`);
     return this.storage.exists(atlasPath);
   }
 
-  get textureGroup(){
+  get textureGroup() {
     return this.yyData.textureGroupId.name;
   }
-  set textureGroup(name:string){
+  set textureGroup(name: string) {
     this.yyData.textureGroupId.name = name;
     this.yyData.textureGroupId.path = `texturegroups/${name}`;
     this.save();
   }
 
   /** Get the array of current frameIds, in their frame order. */
-  get frameIds(){
-    return this.yyData.frames.map(frame=>frame.name);
+  get frameIds() {
+    return this.yyData.frames.map((frame) => frame.name);
   }
 
-  set spriteType(type:SpriteType){
+  set spriteType(type: SpriteType) {
     this.yyData.type = type;
     this.save();
   }
 
-  protected createYyFile(){
+  protected createYyFile() {
     // Create a frameless sprite template
     const firstLayerId = uuidV4();
 
@@ -81,7 +83,7 @@ export class Gms2Sprite extends Gms2ResourceBase {
         {
           ...yyDataLayerDefaults,
           name: firstLayerId,
-        }
+        },
       ],
       name: this.name,
       parent: Gms2Sprite.parentDefault,
@@ -89,7 +91,7 @@ export class Gms2Sprite extends Gms2ResourceBase {
         ...yyDataSequenceDefaults,
         length: new NumberFixed(0), // Update with number of frames
         name: this.name,
-        parent:  this.id,
+        parent: this.id,
         spriteId: this.id,
         tracks: [
           {
@@ -97,9 +99,9 @@ export class Gms2Sprite extends Gms2ResourceBase {
             keyframes: {
               Keyframes: [], // Update once there are frames to add
               resourceType: "KeyframeStore<SpriteFrameKeyframe>",
-              resourceVersion: "1.0"
+              resourceVersion: "1.0",
             },
-          }
+          },
         ],
         xorigin: 0, // Update with the origin of the sprite
         yorigin: 0, // Update with the origin of the sprite
@@ -107,7 +109,7 @@ export class Gms2Sprite extends Gms2ResourceBase {
       textureGroupId: Gms2Sprite.textureGroupIdDefault,
       tags: [],
     };
-    this.storage.writeJson(this.yyPathAbsolute,yyData);
+    this.storage.writeJson(this.yyPathAbsolute, yyData);
   }
 
   /**
@@ -115,7 +117,7 @@ export class Gms2Sprite extends Gms2ResourceBase {
    * Must match the actual dims of the corresponding frames.
    * Adjust origin if the size has changed.
    */
-  protected setDims(width:number,height:number){
+  protected setDims(width: number, height: number) {
     // Get the old height/width and origin for reference
     const oldOriginX = this.yyData.sequence.xorigin;
     const oldOriginY = this.yyData.sequence.yorigin;
@@ -128,94 +130,106 @@ export class Gms2Sprite extends Gms2ResourceBase {
     this.yyData.bbox_right = width;
 
     const originIsUnset = oldHeight == 0 || oldWidth == 0;
-    const dimsHaveChanged = width!=oldWidth || height != oldHeight;
-    if(originIsUnset){
-      this.yyData.sequence.xorigin = Math.floor( width/2 );
-      this.yyData.sequence.yorigin = Math.floor( height/2 );
-    }
-    else if(dimsHaveChanged){
-      this.yyData.sequence.xorigin = Math.floor(oldOriginX/oldWidth  * width );
-      this.yyData.sequence.yorigin = Math.floor(oldOriginY/oldHeight * height);
+    const dimsHaveChanged = width != oldWidth || height != oldHeight;
+    if (originIsUnset) {
+      this.yyData.sequence.xorigin = Math.floor(width / 2);
+      this.yyData.sequence.yorigin = Math.floor(height / 2);
+    } else if (dimsHaveChanged) {
+      this.yyData.sequence.xorigin = Math.floor(
+        (oldOriginX / oldWidth) * width
+      );
+      this.yyData.sequence.yorigin = Math.floor(
+        (oldOriginY / oldHeight) * height
+      );
     }
     return this;
   }
 
   /** Force a layerId. Only updates the YY file, and only if there is only 1 layer. */
-  setLayerId(layerId:string){
-    assert(this.yyData.layers.length===1,"Cannot force the layerId if only one layer present.");
+  setLayerId(layerId: string) {
+    assert(
+      this.yyData.layers.length === 1,
+      "Cannot force the layerId if only one layer present."
+    );
     this.yyData.layers[0].name = layerId;
     return this.save();
   }
 
-  addFrame(imagePath:string,frameGuid?:string,keyframeId?:string){
+  addFrame(imagePath: string, frameGuid?: string, keyframeId?: string) {
     frameGuid ||= uuidV4();
     keyframeId ||= uuidV4();
     const keyFrames = this.yyData.sequence.tracks[0].keyframes.Keyframes;
     this.yyData.sequence.length = new NumberFixed(0);
-    const framePath = paths.join(this.yyDirAbsolute,`${frameGuid}.png`);
-    const frameLayerFolder = paths.join(this.yyDirAbsolute,'layers',frameGuid);
+    const framePath = paths.join(this.yyDirAbsolute, `${frameGuid}.png`);
+    const frameLayerFolder = paths.join(
+      this.yyDirAbsolute,
+      "layers",
+      frameGuid
+    );
     const layerId = this.yyData.layers[0].name;
-    const frameLayerImagePath = paths.join(frameLayerFolder,`${layerId}.png`);
+    const frameLayerImagePath = paths.join(frameLayerFolder, `${layerId}.png`);
     this.storage.ensureDir(frameLayerFolder);
-    this.storage.copyFile(imagePath,framePath);
-    this.storage.copyFile(imagePath,frameLayerImagePath);
+    this.storage.copyFile(imagePath, framePath);
+    this.storage.copyFile(imagePath, frameLayerImagePath);
     this.yyData.frames.push({
       compositeImage: {
-        FrameId:{
+        FrameId: {
           name: frameGuid,
-          path: this.id.path
+          path: this.id.path,
         },
-        LayerId:null,
-        resourceVersion:"1.0",
-        name:"",
+        LayerId: null,
+        resourceVersion: "1.0",
+        name: "",
         tags: [],
-        resourceType:"GMSpriteBitmap"
+        resourceType: "GMSpriteBitmap",
       },
-      images: [{
-        FrameId:{
-          name: frameGuid,
-          path: this.id.path
+      images: [
+        {
+          FrameId: {
+            name: frameGuid,
+            path: this.id.path,
+          },
+          LayerId: {
+            name: layerId,
+            path: this.id.path,
+          },
+          resourceVersion: "1.0",
+          name: "",
+          tags: [],
+          resourceType: "GMSpriteBitmap",
         },
-        LayerId:{
-          name: layerId,
-          path: this.id.path
-        },
-        resourceVersion:"1.0",
-        name:"",
-        tags: [],
-        resourceType:"GMSpriteBitmap"
-      }],
+      ],
       name: frameGuid,
-      parent:this.id,
-      resourceVersion:"1.0",
-      tags:[],
-      resourceType:"GMSpriteFrame"
+      parent: this.id,
+      resourceVersion: "1.0",
+      tags: [],
+      resourceType: "GMSpriteFrame",
     });
     keyFrames.push({
       id: keyframeId,
-      Key: new NumberFixed(this.yyData.frames.length-1),
+      Key: new NumberFixed(this.yyData.frames.length - 1),
       Length: new NumberFixed(1),
       Stretch: false,
       Disabled: false,
       IsCreationKey: false,
-      Channels:{
-        '0':{
-          Id:{
+      Channels: {
+        "0": {
+          Id: {
             name: frameGuid,
-            path: this.id.path
+            path: this.id.path,
           },
-          resourceVersion:"1.0",
-          resourceType:"SpriteFrameKeyframe"
-        }
+          resourceVersion: "1.0",
+          resourceType: "SpriteFrameKeyframe",
+        },
       },
-      resourceVersion:"1.0",
-      resourceType:"Keyframe<SpriteFrameKeyframe>"
+      resourceVersion: "1.0",
+      resourceType: "Keyframe<SpriteFrameKeyframe>",
     });
     this.yyData.sequence.length = new NumberFixed(this.yyData.frames.length);
     return this.save();
   }
 
-  clearFrames(){
+  clearFrames() {
     logDebug(`clearing frames for sprite ${this.name}`);
     this.yyData.frames = [];
     this.yyData.sequence.tracks[0].keyframes.Keyframes = [];
@@ -226,43 +240,46 @@ export class Gms2Sprite extends Gms2ResourceBase {
    * Force the frames of this sprite to match the images
    * within a folder (non-recursive)
    */
-  replaceFrames(spriteDirectory:string){
+  replaceFrames(spriteDirectory: string) {
     logDebug(`replacing frames from source ${spriteDirectory}`);
     const sprite = new Spritely(spriteDirectory);
     // Ensure that the sizes match
-    this.setDims(sprite.width as number,sprite.height as number);
+    this.setDims(sprite.width as number, sprite.height as number);
     // Replace all the frames
 
     // Replace all frames, but keep the existing IDs and ID
     // order where possible. (Minimizes useless git history changes.)
-    const layersRoot = paths.join(this.yyDirAbsolute,'layers');
+    const layersRoot = paths.join(this.yyDirAbsolute, "layers");
     this.storage.ensureDir(layersRoot);
     this.storage.emptyDir(layersRoot);
     const oldFrameIds = this.frameIds;
     const track = this.yyData.sequence.tracks[0];
-    const oldKeyframeIds = track.keyframes.Keyframes.map(frame=>frame.id);
-    logDebug(`old frameIds: ${oldFrameIds.join(', ')}`);
-    const oldFrames = this.storage
-      .listFiles(this.yyDirAbsolute,false,['png']);
-    for(const frame of oldFrames){
+    const oldKeyframeIds = track.keyframes.Keyframes.map((frame) => frame.id);
+    logDebug(`old frameIds: ${oldFrameIds.join(", ")}`);
+    const oldFrames = this.storage.listFiles(this.yyDirAbsolute, false, [
+      "png",
+    ]);
+    for (const frame of oldFrames) {
       this.storage.deleteFile(frame);
       logDebug(`deleted old frame ${frame}`);
     }
 
     this.clearFrames();
     // Add each new frame, updating the yyData as we go.
-    for(const [i,subimagePath] of sprite.paths.entries()){
+    for (const [i, subimagePath] of sprite.paths.entries()) {
       const frameId = oldFrameIds[i];
       const keyframeId = oldKeyframeIds[i];
-      logDebug(`adding frame ${i} using id ${frameId} from image at ${subimagePath}`);
-      this.addFrame(subimagePath,frameId,keyframeId);
+      logDebug(
+        `adding frame ${i} using id ${frameId} from image at ${subimagePath}`
+      );
+      this.addFrame(subimagePath, frameId, keyframeId);
     }
     return this;
   }
 
-  static get textureGroupIdDefault(){
+  static get textureGroupIdDefault() {
     return {
-      name: 'Default',
+      name: "Default",
       path: "texturegroups/Default",
     };
   }
@@ -274,9 +291,15 @@ export class Gms2Sprite extends Gms2ResourceBase {
    *                          search for png images within that directory
    *                          and sort them alphabetically.
    */
-  static create(subimageDirectory:string,storage:Gms2Storage,spriteName?:string) {
-    return new Gms2Sprite(spriteName||paths.subfolderName(subimageDirectory),storage,true)
-      .replaceFrames(subimageDirectory);
+  static create(
+    subimageDirectory: string,
+    storage: Gms2Storage,
+    spriteName?: string
+  ) {
+    return new Gms2Sprite(
+      spriteName || paths.subfolderName(subimageDirectory),
+      storage,
+      true
+    ).replaceFrames(subimageDirectory);
   }
-
 }
