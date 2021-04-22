4d1024bd4b0fad412873de6a260d2a9422804f9f
Adam Coster <adam@bscotch.net>
2021-04-22 15:02:13 -0500

    chore: Add prettier and remove appearance-related eslint configs. Keep code looking nice automatically without having to make manual cosmetic fixes!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,32 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "mocha": true
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint",
-    "prettier"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "rules": {
-    "@typescript-eslint/indent": [2,2],
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/type-annotation-spacing": "off",
-    "@typescript-eslint/member-delimiter-style":"off",
-    "@typescript-eslint/explicit-function-return-type":"off",
-    'semi':'error',
-    "no-trailing-spaces": "error",
-    "no-unused-vars": "off",
-    '@typescript-eslint/no-unused-vars': ["warn", { args: 'none' }],
-    "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/ban-types": "error",
-    "camelcase": "off",
-    "@typescript-eslint/consistent-type-assertions": "error",
-    "no-array-constructor": "off",
-    "@typescript-eslint/no-array-constructor": "error",
-    "no-empty":"off",
-    "no-empty-function": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-empty-function": "error",
-    "@typescript-eslint/no-empty-interface": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-inferrable-types": "error",
-    "@typescript-eslint/no-misused-new": "error",
-    "@typescript-eslint/no-namespace": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-this-alias": "error",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "error",
-    "@typescript-eslint/no-var-requires": "error",
-    "@typescript-eslint/prefer-namespace-keyword": "error",
-    "@typescript-eslint/triple-slash-reference": "error",
-    "no-var": "error",
-    "prefer-const": "error",
-    "prefer-rest-params": "error",
-    "prefer-spread": "error"
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+    'space-before-function-paren': 'off',
+    'key-spacing': 'off',
+    quotes: 'off',
+    eqeqeq: 'off',
+    camelcase: 'off',
+    'require-await': 'error',
+    'no-unused-vars': 'off',
+    'no-empty': 'off',
+    'getter-return': 'off',
+    'no-constant-condition': 'off',
+    'no-prototype-builtins': 'off',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': ['error'],
   },
-  "overrides": [
-    {
-      "files": ["*.js"],
-      "rules": {
-        "@typescript-eslint/no-var-requires": "off"
-      }
-    }
-  ]
 };
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
