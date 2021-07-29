db4e651ced5d1f7cb9a4afccaf4fa920b9877837
Adam Coster <adam@bscotch.net>
2021-07-29 17:39:48 -0500

    feat: Add utility methods for determining relative proximity of a child to a parent path.

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -1,14 +1,81 @@
 import nodePath from 'path';
 
 /**
+ * Given a path with any style of separators,
+ * return the same path with POSIX-style separators.
+ */
+function asPosixPath(pathString: string) {
+  const parts = pathString.split(/[/\\]+/g);
+  const withPosixSeps = nodePath.posix.join(...parts);
+  // When converting a Windows absolute path, e.g. C:// must become /c/
+  return withPosixSeps.replace(/^([a-z])\/\//i, '/$1/');
+}
+
+function trimTrailingSlash(pathString: string) {
+  return pathString.replace(/[/\\]+$/, '');
+}
+
+function pathParts(pathString: string) {
+  return pathString.split(/[/\\]+/g);
+}
+
+/**
+ * Get the number of directory levels between a child and
+ * parent. Treats both as paths for directories and trims trailing slashes.
+ * Ignores case!
+ *
+ * For example:
+ *
+ * - If the parent and child are the same, returns `0`.
+ * - If the child is an immediate child of the parent, returns `1`.
+ * - If the child is not inside parent at all, returns `NaN`.
+ */
+function generationsFromParent(parent: string, child: string) {
+  const parentParts = pathParts(trimTrailingSlash(parent.toLocaleLowerCase()));
+  const childParts = pathParts(trimTrailingSlash(child.toLocaleLowerCase()));
+  let common = 0;
+  while (
+    common < parentParts.length &&
+    common < childParts.length &&
+    parentParts[common] == childParts[common]
+  ) {
+    common++;
+  }
+  if (common == 0) {
+    return NaN;
+  }
+  return childParts.length - common;
+}
+
+/**
+ * For a set of parents and a child, return the parent closest
+ * to that child (if there is one, else return `undefined`).
+ */
+function findClosestParent(parents: string[], child: string) {
+  let closestParent: string | undefined;
+  let closestParentGenerationsAway = Infinity;
+  for (const parent of parents) {
+    const generationsAway = generationsFromParent(parent, child);
+    if (
+      !isNaN(generationsAway) &&
+      generationsAway < closestParentGenerationsAway
+    ) {
+      closestParent = parent;
+      closestParentGenerationsAway = generationsAway;
+    }
+  }
+  return closestParent;
+}
+
+/**
  * Pass to `.sort()` got an array of paths to get
  * them sorted by *least* to *most* specific (i.e.
  * fewest to most subdirs) and alphabetically by
  * directory within a specificity tier.
  */
 function pathSpecificitySort(path1: string, path2: string) {
-  const path1Parts = path1.split(/[\\/]+/);
-  const path2Parts = path2.split(/[\\/]+/);
+  const path1Parts = pathParts(path1);
+  const path2Parts = pathParts(path2);
   if (path1Parts.length != path2Parts.length) {
     return path1Parts.length - path2Parts.length;
   }
@@ -40,25 +107,10 @@ function heirarchy(path: string) {
 }
 
 /**
- * Given a path with any style of separators,
- * return the same path with POSIX-style separators.
- */
-function asPosixPath(pathString: string) {
-  const parts = pathString.split(/[/\\]+/g);
-  const withPosixSeps = nodePath.posix.join(...parts);
-  // When converting a Windows absolute path, e.g. C:// must become /c/
-  return withPosixSeps.replace(/^([a-z])\/\//i, '/$1/');
-}
-
-function trimTrailingSlash(pathString: string) {
-  return pathString.replace(/[/\\]+$/, '');
-}
-
-/**
  * Given the path to a directory, return the final subdirectory name.
  * E.g. from /hello/world/ return "world".
  */
-export function subfolderName(directoryPath: string) {
+function subfolderName(directoryPath: string) {
   return nodePath.parse(trimTrailingSlash(directoryPath)).base;
 }
 
@@ -71,7 +123,8 @@ export default {
   pathSpecificitySort,
   heirarchy,
   asPosixPath,
-  trimTrailingSlash,
   subfolderName,
   changeExtension,
+  generationsFromParent,
+  findClosestParent,
 };
