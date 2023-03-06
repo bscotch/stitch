b03d8874e45a505318ac579a1dad526db3a87c9e
Adam Coster <adam@bscotch.net>
2023-03-06 15:48:20 -0600

    meta: Added GitHub workflow for building and publishing Stitch Desktop

---
+++ scripts/set-release-version.mjs
@@ -0,0 +1,24 @@
+// Set the RELEASE_VERSION env var to the version of a package
+
+import fs from 'fs/promises';
+
+const packageDir = process.argv[2];
+
+const manifestPath = `${packageDir}/package.json`;
+
+try {
+	await fs.stat(manifestPath);
+} catch (err) {
+	console.error(`No package.json in ${packageDir}`);
+	process.exit(1);
+}
+
+const manifest = JSON.parse(await fs.readFile(manifestPath, 'utf8'));
+
+const version = manifest.version;
+
+await fs.appendFile(
+	/** @type {string} */
+	(process.env.GITHUB_ENV),
+	`RELEASE_VERSION=${version}`,
+);
