c75b23ef826c0e060e943acc23e5f89fab4edb2c
Adam Coster <adam@bscotch.net>
2023-03-06 17:25:43 -0600

    meta: Resolved broken build step

--- scripts/set-release-version.mjs
+++ scripts/set-release-version.mjs
@@ -7,18 +7,23 @@ const packageDir = process.argv[2];
 const manifestPath = `${packageDir}/package.json`;
 
 try {
-	await fs.stat(manifestPath);
+  await fs.stat(manifestPath);
 } catch (err) {
-	console.error(`No package.json in ${packageDir}`);
-	process.exit(1);
+  console.error(`No package.json in ${packageDir}`);
+  process.exit(1);
 }
 
 const manifest = JSON.parse(await fs.readFile(manifestPath, 'utf8'));
 
 const version = manifest.version;
 
+console.log({
+  name: manifest.name,
+  version,
+});
+
 await fs.appendFile(
-	/** @type {string} */
-	(process.env.GITHUB_ENV),
-	`RELEASE_VERSION=${version}`,
+  /** @type {string} */
+  (process.env.GITHUB_ENV),
+  `RELEASE_VERSION=${version}`,
 );
