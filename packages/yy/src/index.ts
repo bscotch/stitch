d0d526d2da4491fb51dd39b2580e61e8849b34b6
Adam Coster <adam@bscotch.net>
2023-03-11 13:40:53 -0600

    feat: Now exporting all YY types

--- packages/yy/src/index.ts
+++ packages/yy/src/index.ts
@@ -1,8 +1,8 @@
+export * from './Yy.js';
 export * from './types/YyBase.js';
 export * from './types/YyObject.js';
-export * from './types/Yyp.js';
 export * from './types/YyRoom.js';
 export * from './types/YyScript.js';
 export * from './types/YySound.js';
 export * from './types/YySprite.js';
-export { Yy } from './Yy.js';
+export * from './types/Yyp.js';
