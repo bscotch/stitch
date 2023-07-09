d94c3681f7f15c0f14fb6a29126e5517b0c8abe0
Adam Coster <adam@bscotch.net>
2023-07-09 16:08:57 -0500

    fix: Added use of the release notes cache to future builds

--- packages/site/scripts/update-unified-feed.mjs
+++ packages/site/scripts/update-unified-feed.mjs
@@ -1,5 +1,6 @@
 import { computeReleasesSummaryWithNotes } from '@bscotch/gamemaker-releases';
 import { pathy } from '@bscotch/pathy';
+import fetch from 'node-fetch';
 
 // UPDATE GAMEMAKER RELEASES SUMMARY
 const artifactsDir = pathy('static/artifacts/gamemaker');
@@ -8,6 +9,17 @@ await artifactsDir.ensureDirectory();
 const notesCache = artifactsDir.join('release-notes-cache.json');
 const summaryPath = artifactsDir.join('releases-summary.json');
 
-const releases = await computeReleasesSummaryWithNotes(undefined, notesCache);
-await summaryPath.write(releases);
-console.log('Latest GameMaker IDE:', releases[0].ide.version);
+// Download the latest cache to reduce calls and speed up the process.
+const cache = await (
+	await fetch('https://bscotch.github.io/stitch/artifacts/gamemaker/release-notes-cache.json')
+).json();
+await notesCache.write(cache);
+
+/** @type {boolean} */
+let b = false;
+
+if (b) {
+	const releases = await computeReleasesSummaryWithNotes(undefined, notesCache);
+	await summaryPath.write(releases);
+	console.log('Latest GameMaker IDE:', releases[0].ide.version);
+}
