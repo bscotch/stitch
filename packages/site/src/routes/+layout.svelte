5e64d9d94b05db4c3affa4d273c4a9c0ebd5ef57
Adam Coster <adam@bscotch.net>
2023-07-09 12:39:44 -0500

    meta: Created skeleton project for a Stitch website

---
+++ packages/site/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Logo from '$lib/assets/stitch-logo-small.webp';
+</script>
+
+<nav>
+	<a href="/" title="Go home">
+		<img src={Logo} alt="SvelteKit" />
+		<span class="name">Stitch</span>
+	</a>
+</nav>
+
+<slot />
+
+<style>
+	nav a {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+	}
+	nav a:hover {
+		text-decoration: underline;
+	}
+	nav a .name {
+		font-weight: bold;
+		color: var(--color-text);
+		padding-inline: 0.5rem;
+	}
+	img {
+		width: 2rem;
+		height: auto;
+		display: inline-block;
+	}
+</style>
---
+++ packages/site/src/routes/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Logo from '$lib/assets/stitch-logo-small.webp';
+  import { fade } from 'svelte/transition';
+</script>
+
+<main transition:fade>
+  <article>
+    <header>
+      <figure>
+        <div class="background" />
+        <img src={Logo} alt="Stitch Logo" />
+      </figure>
+      <h1>Stitch</h1>
+    </header>
+    <p>
+      A toolkit for managing <a href="https://gamemaker.io/en">GameMaker</a> projects
+    </p>
+    <footer> by <a href="https://www.bscotch.net">Butterscotch Shenanigans</a>
+    </footer>
+  </article>
+
+</main>
+
+<style>
+  main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 3em;
+  }
+  figure {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+  }
+
+  .background {
+    position: absolute;
+    width: 100%;
+    aspect-ratio: 1/1;
+    animation: breathing 3s ease-in-out infinite normal;
+    background-image: linear-gradient(
+      45deg,
+      var(--color-logo-yellow) 50%,
+      var(--color-logo-purple) 50%
+    );
+    border-radius: 50%;
+    filter: blur(4em);
+  }
+
+  img{
+    z-index: 1;
+  }
+
+  img {
+    filter: drop-shadow(0.25em 0.25em 0.5em black);
+  }
+
+  h1 {
+    text-shadow: 0.1em 0.1em 0.2em black;
+  }
+
+  @keyframes breathing {
+    0% {
+      transform: scale(1.1);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1.1);
+    }
+  }
+</style>
