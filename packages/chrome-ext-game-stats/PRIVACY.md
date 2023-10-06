f8d9977f2394f9c6814a9947a5e87d302e7dd080
Adam Coster <adam@bscotch.net>
2023-10-06 14:32:02 -0500

    feat: Drafted a chrome extension to replace cumbersome bookmarklets

---
+++ packages/chrome-ext-game-stats/PRIVACY.md
@@ -0,0 +1,7 @@
+# Privacy Policy
+
+This extension collects no user data, nor sends any data to any servers.
+
+This extension adds data and functionality to certain websites, and therefore has access to the content on those sites. It does not, however, do anything with that data besides adding local functionality.
+
+Any data created by this extension is only available within the browser unless you download it or copy-paste it into some other application.
---
+++ packages/chrome-ext-game-stats/steam-likes.js
@@ -0,0 +1,72 @@
+async function addButtons() {
+  // The URL looks like this: https://steamlikes.co/app/1401730
+  // Make sure we're on the right host
+
+  // Get the steamId from the URL
+  const steamId = window.location.pathname.split('/')[2];
+
+  // Grab the same data that the SteamLikes website uses. Have to do it
+  // while ON the site because of CORS, hence this bookmarklet!
+  const url = 'https://steamlikes.co/app/a/' + steamId + '/likesstatictics';
+
+  const data = await fetch(url, {})
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error(err);
+      return { data: { datasets: [{ data: [] }] } };
+    });
+
+  // The date values are just the month and day, so we need to add the year.
+  // (This will get janky around the new year!)
+  const lastDate = new Date(data.data.labels.at(-1));
+  lastDate.setFullYear(new Date().getFullYear());
+
+  const likes = data.data.datasets[0].data.map((likes, i) => ({
+    date: new Date(lastDate.getTime() - (13 - i) * 24 * 60 * 60 * 1000),
+    likes,
+  }));
+
+  const asCsv = likes
+    .map((entry) => entry.date.toISOString().split('T')[0] + ',' + entry.likes)
+    .join('\\n');
+  const asJson = JSON.stringify(likes).replace(/"/g, '&quot;');
+  const copyAsCsv = "navigator.clipboard.writeText('" + asCsv + "')";
+  const copyAsJson = "navigator.clipboard.writeText('" + asJson + "')";
+  const copyAsTsv =
+    "navigator.clipboard.writeText('" + asCsv.replace(/,/g, '\\t') + "')";
+
+  const tabEl = document.querySelector('#myTab');
+  assert(tabEl, 'Could not find #myTab');
+
+  // Add a sibling after the tabs that adds liks to "Copy as: csv | json"
+  const copyEl = document.createElement('div');
+  copyEl.id = 'stitch-copy-container';
+  copyEl.innerHTML =
+    '<a href="https://bscotch.github.io/stitch/steam-tools">Stitch</a>: <button onclick="' +
+    copyAsCsv +
+    '">Copy CSV</button> <button onclick="' +
+    copyAsTsv +
+    '">Copy TSV</button>  <button onclick="' +
+    copyAsJson +
+    '">Copy JSON</button>';
+  copyEl.style = 'margin: 1em 0.5em; font-size: 1.2rem; font-weight:bold;';
+
+  const existing = document.querySelector('#stitch-copy-container');
+  if (existing) {
+    existing.remove();
+  }
+  tabEl.after(copyEl);
+}
+
+/**
+ * @param {any} claim
+ * @param {string} msg
+ * @returns {asserts claim}
+ */
+function assert(claim, msg) {
+  if (!claim) {
+    throw new Error(msg);
+  }
+}
+
+addButtons();
