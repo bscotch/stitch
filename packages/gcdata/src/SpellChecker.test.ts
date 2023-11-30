7411b179924c683e1629f0f37ed4d42eb751997a
Adam Coster <adam@bscotch.net>
2023-11-30 12:04:26 -0600

    partial: Drafted conversion of mote names to dictionary entries

--- packages/gcdata/src/SpellChecker.test.ts
+++ packages/gcdata/src/SpellChecker.test.ts
@@ -1,7 +1,8 @@
-import { checker } from "./SpellChecker.js";
-
-describe('SpellChecker', function(){
-    it('can spellcheck', function(){
-      console.log('COLOUR', checker.suggest('COLOUR'));
-    });
-})
+import { GameChanger } from './GameChanger.js';
+import { SpellChecker } from './SpellChecker.js';
+const checker = new SpellChecker((await GameChanger.from('Crashlands2'))!);
+describe('SpellChecker', async function () {
+  it('can spellcheck', function () {
+    // console.log('COLOUR', checker.suggest('COLOUR'));
+  });
+});
--- packages/gcdata/src/SpellChecker.ts
+++ packages/gcdata/src/SpellChecker.ts
@@ -1,5 +1,86 @@
 import dictionary from 'dictionary-en';
 import nspell from 'nspell';
+import type { GameChanger } from './GameChanger.js';
+import { normalizePointer, resolvePointerInSchema } from './util.js';
 
-export const checker = nspell(Buffer.from(dictionary.aff), Buffer.from(dictionary.dic));
+export class SpellChecker {
+  protected checker = nspell(
+    Buffer.from(dictionary.aff),
+    Buffer.from(dictionary.dic),
+  );
 
+  constructor(
+    public readonly gameChanger: GameChanger,
+    protected options: {
+      /** Motes with these schema IDs will not have their names added to the dictionary */
+      skipSchemas?: string[];
+    } = {},
+  ) {
+    this.reload();
+  }
+
+  reload() {
+    this.checker = nspell(
+      Buffer.from(dictionary.aff),
+      Buffer.from(dictionary.dic),
+    );
+    const words = this.listMoteNameWords();
+    console.log(JSON.stringify(words, null, 2), words.length);
+  }
+
+  /** Get the list of localized Mote Names, broken into "words" for use in the dictionary. */
+  protected listMoteNameWords() {
+    const names: string[] = [];
+
+    // Find all mote names that are meant to be localized, and
+    // add those names to the spell checker.
+    const motes = this.gameChanger.working.listMotes();
+    for (const mote of motes) {
+      if (this.options.skipSchemas?.includes(mote.schema_id)) continue;
+      const schema = this.gameChanger.working.getSchema(mote.schema_id);
+      const namePointer = schema?.name;
+      if (!schema || !namePointer) continue;
+      // Need to check one level up to see if this is an L10n field.
+      const parentPointer = normalizePointer(namePointer);
+      parentPointer.pop();
+      const nameSchema = resolvePointerInSchema(
+        parentPointer,
+        mote,
+        this.gameChanger.working,
+      );
+      if (
+        nameSchema &&
+        'format' in nameSchema &&
+        nameSchema.format === 'l10n' &&
+        !mote.data.wip?.draft
+      ) {
+        names.push(this.gameChanger.working.getMoteName(mote.id)!);
+      }
+    }
+
+    const words = new Set<string>();
+    for (const name of names) {
+      const parts = name.trim().split(/\s+/g);
+      for (let part of parts) {
+        // Remove any apostrophes
+        part = part.replace(/'.*/g, '');
+        // Remove any trailing punctuation
+        part = part.replace(/\s*[!?)\]-]+$/g, '');
+        // Remove any leading punctuation
+        part = part.replace(/^[!?([-]+\s*/g, '');
+
+        const skip =
+          part.length < 2 ||
+          part.match(/^\d+|\d+x\d+|.*\/.*$/) ||
+          this.checker.correct(part);
+        if (!skip) {
+          words.add(part);
+        }
+      }
+    }
+    // Sort by lower-case
+    return [...words].sort((a, b) =>
+      a.toLowerCase().localeCompare(b.toLowerCase()),
+    );
+  }
+}
