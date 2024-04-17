1384f8507e39ec535f2d64491b05a049f89ea69c
Adam Coster <adam@bscotch.net>
2024-04-17 11:00:25 -0500

    meta: Removed legacy deps

--- packages/gcdata/scripts/compile-dict.mjs
+++
@@ -1,7 +0,0 @@
-import dictionary from 'dictionary-en';
-import fsp from 'fs/promises';
-
-await fsp.writeFile(
-  './src/dict.ts',
-  `export const aff = \`${dictionary.aff}\`;\nexport const dic = \`${dictionary.dic}\`;`,
-);
