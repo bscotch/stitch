924c60e14060e1a67c07a32bf13bb98549c006d6
Adam Coster <adam@bscotch.net>
2025-08-06 15:52:57 -0500

    fix: Bumped config's dependencies and replaced external zod-to-json-schema with native one in Zod4

--- packages/config/scripts/update-schemas.mjs
+++ packages/config/scripts/update-schemas.mjs
@@ -1,16 +1,19 @@
 import { pathy } from '@bscotch/pathy';
-import { zodToJsonSchema } from 'zod-to-json-schema';
+// import { zodToJsonSchema } from 'zod-to-json-schema';
+import { z } from 'zod';
 import { jsonSchemaUrl, stitchConfigSchema } from '../dist/schema.js';
 
 const schemasDir = pathy('./schemas');
 await schemasDir.ensureDirectory();
 
-const asSchema = zodToJsonSchema(stitchConfigSchema);
+const asSchema = z.toJSONSchema(stitchConfigSchema, {
+  target: 'draft-7',
+}); //zodToJsonSchema(stitchConfigSchema);
 // Remove the '$schema' property, since it's only needed for the literals,
 // and add the $id property
 // @ts-expect-error
 delete asSchema.properties.$schema;
-// @ts-expect-error
+
 asSchema.$id = jsonSchemaUrl;
 
 await pathy(schemasDir).join(`stitch.config.schema.json`).write(asSchema);
--- packages/config/src/schema.ts
+++ packages/config/src/schema.ts
@@ -20,41 +20,42 @@ export const newSoundDefaultsSchema = z.object({
 });
 
 export type GameConsoleLineStyle = z.infer<typeof gameConsoleLineStyleSchema>;
-export const gameConsoleLineStyleSchema = z
-  .object({
-    base: z
-      .string()
-      .optional()
-      .describe(
-        "Base style to apply to the matching line (e.g. 'color: #808080; font-weight: bold;')",
-      ),
-    description: z
-      .string()
-      .optional()
-      .describe('A description of the rule, for debugging purposes.'),
-    pattern: z
-      .string()
-      .describe(
-        'A regex pattern to match against the line. Named capture groups can be referenced in styles. Special names `_GMFILE_` and `_GMLINE_` may be used to enable linking to that part of the project code.',
-      ),
-    caseSensitive: z
-      .boolean()
-      .optional()
-      .describe(
-        'If true, the pattern will be treated as case-sensitive. Default is false.',
-      ),
-    styles: z
-      .record(z.string().describe('CSS string to apply to this capture group.'))
-      .optional()
-      .describe(
-        "A map of CSS styles to apply to named capture groups in the line, as a CSS string (e.g. 'color: #808080').",
-      ),
-  })
-  .passthrough();
+export const gameConsoleLineStyleSchema = z.looseObject({
+  base: z
+    .string()
+    .optional()
+    .describe(
+      "Base style to apply to the matching line (e.g. 'color: #808080; font-weight: bold;')",
+    ),
+  description: z
+    .string()
+    .optional()
+    .describe('A description of the rule, for debugging purposes.'),
+  pattern: z
+    .string()
+    .describe(
+      'A regex pattern to match against the line. Named capture groups can be referenced in styles. Special names `_GMFILE_` and `_GMLINE_` may be used to enable linking to that part of the project code.',
+    ),
+  caseSensitive: z
+    .boolean()
+    .optional()
+    .describe(
+      'If true, the pattern will be treated as case-sensitive. Default is false.',
+    ),
+  styles: z
+    .record(
+      z.string(),
+      z.string().describe('CSS string to apply to this capture group.'),
+    )
+    .optional()
+    .describe(
+      "A map of CSS styles to apply to named capture groups in the line, as a CSS string (e.g. 'color: #808080').",
+    ),
+});
 
 export type GameConsoleStyle = z.infer<typeof gameConsoleStyleSchema>;
 export const gameConsoleStyleSchema = z
-  .object({
+  .looseObject({
     base: z
       .string()
       .optional()
@@ -68,23 +69,22 @@ export const gameConsoleStyleSchema = z
         "An array of style rules to apply to lines of the game's STDOUT/STDERR. The first matching rule is used for a given line.",
       ),
   })
-  .passthrough()
   .describe(
     "Styling rules for the game's STDOUT/STDERR for compatible runners",
   );
 
 export type StitchConfig = z.infer<typeof stitchConfigSchema>;
 export const stitchConfigSchema = z
-  .object({
+  .looseObject({
     $schema: z.literal(jsonSchemaUrl).default(jsonSchemaUrl),
     textureGroupAssignments: z
-      .record(z.string())
+      .record(z.string(), z.string())
       .default({})
       .describe(
         'A map of resource tree paths to texture groups name. Supported Stitch utilities will use this to assign sprites in those paths (recursively) to the specified texture group.',
       ),
     audioGroupAssignments: z
-      .record(z.string())
+      .record(z.string(), z.string())
       .default({})
       .describe(
         'A map of resource tree paths to audio groups name. Supported Stitch utilities will use this to assign sounds in those paths (recursively) to the specified audio group.',
@@ -96,30 +96,28 @@ export const stitchConfigSchema = z
         'When set, supported Stitch utilities will preferentially use this GameMaker runtime version.',
       ),
     newSpriteRules: z
-      .object({ allowedNames })
-      .passthrough()
+      .looseObject({ allowedNames })
+
       .optional()
       .describe(
         'Rules for creating new sprite resources, followed by supported Stitch utilities.',
       ),
     newSoundRules: z
-      .object({
+      .looseObject({
         allowedNames,
         defaults: z
-          .record(newSoundDefaultsSchema)
+          .record(z.string(), newSoundDefaultsSchema)
           .optional()
           .describe(
             'Default properties for new sound assets, by name pattern. E.g. `{".*":{ mono: true}}` defaults all new sounds to mono. The first matching pattern is used.',
           ),
       })
-      .passthrough()
       .optional()
       .describe(
         'Rules for creating new sound resources, followed by supported Stitch utilities.',
       ),
     gameConsoleStyle: gameConsoleStyleSchema.optional(),
   })
-  .passthrough()
   .describe(
     'Stitch configuration schema. Stitch utilities may support subsets of this configuration.',
   );
