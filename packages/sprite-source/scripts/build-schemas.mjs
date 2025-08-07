f8c00b659f8b906789749b3b2da07c06c36d49d4
Adam Coster <adam@bscotch.net>
2025-08-07 16:52:56 -0500

    meta: Added sprite-source schemas to its own project for standalone referencing.

---
+++ packages/sprite-source/scripts/build-schemas.mjs
@@ -0,0 +1,33 @@
+import { pathy } from '@bscotch/pathy';
+import { z } from 'zod';
+import {
+  spriteDestConfigSchema,
+  spriteSourceConfigSchema,
+  spritesInfoSchema,
+} from '../dist/index.js';
+
+const schemas = [
+  {
+    schema: spriteDestConfigSchema,
+    name: 'Sprite Import Configuration',
+    filename: 'stitch.sprite-imports.schema.json',
+  },
+  {
+    schema: spriteSourceConfigSchema,
+    name: 'Sprite Source Configuration',
+    filename: 'stitch.sprite-source.schema.json',
+  },
+  {
+    schema: spritesInfoSchema,
+    name: 'Sprite Cache',
+    filename: 'stitch.sprite-cache.schema.json',
+  },
+];
+
+const dir = pathy('schemas');
+await dir.ensureDirectory();
+
+for (const { schema, name, filename } of schemas) {
+  const jsonSchema = z.toJSONSchema(schema, { target: 'draft-7' });
+  await pathy(filename, dir).write(jsonSchema);
+}
